70e15534606a7e653f14e562fc9ce4239273389c
Casey Gollan <casey@bullshit.systems>
2016-01-05 00:55:37 -0500

    homepage tweak

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,18 +1,18 @@
 <?php snippet('header') ?>
 
-<div class="row">
+<div class="header row">
 	<?php snippet('hero') ?>
 </div>
 
 <div class="row peek">
-	<div class="col-xs-6 col-sm-4">
+	<div class="col-xs-6 col-sm-5">
 		<?php snippet('page-peek', array('section' => 'events')) ?>
 	</div>
-	<div class="col-xs-6 col-sm-4">
+	<div class="col-xs-6 col-sm-5">
 		<h2>Recess Studio</h2>
 		<?php snippet('page-peek', array('section' => 'studio')) ?>
 	</div>
-	<div class="col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-0">
+	<div class="col-xs-6 col-sm-2">
 		<h2>Intermission</h2>
 		<?php snippet('page-peek', array('section' => 'intermission')) ?>
 	</div>
