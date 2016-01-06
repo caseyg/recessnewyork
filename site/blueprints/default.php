cfd80a0391d4cf571fa8aecf2c734252263381f7
Casey Gollan <hello@caseyagollan.com>
2016-01-06 17:48:36 -0500

    blueprint cleanup

--- site/blueprints/default.php
+++ site/blueprints/default.php
@@ -10,19 +10,6 @@ fields:
   subtitle:
     label: Subtitle
     type:  text
-  herobackgroundcolor:
-  	label: Hero Background Color
-  	type: color
-  carousel:
-    label: Carousel
-    type: structure
-    entry: >
-      {{page}}<br />
-    fields:
-      page:
-        label: Page
-        type: select
-        options: children
   text:
     label: Text
     type:  textarea
--- site/blueprints/events.php
+++ site/blueprints/events.php
@@ -1,6 +1,6 @@
 <?php if(!defined('KIRBY')) exit ?>
 
-title: Page
+title: Events
 deletable: false
 pages:
   template: event
@@ -11,22 +11,3 @@ fields:
   title:
     label: Title
     type:  text
-  subtitle:
-    label: Subtitle
-    type:  text
-  herobackgroundcolor:
-  	label: Hero Background Color
-  	type: color
-  carousel:
-    label: Carousel
-    type: structure
-    entry: >
-      {{page}}<br />
-    fields:
-      page:
-        label: Page
-        type: select
-        options: children
-  text:
-    label: Text
-    type:  textarea
