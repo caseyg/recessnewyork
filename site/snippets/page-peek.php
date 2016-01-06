9624631db21e55417263555290618c23747464e6
Casey Gollan <hello@caseyagollan.com>
2016-01-06 16:21:30 -0500

    homepage col width + crop, fixes #6

--- site/snippets/page-peek.php
+++ site/snippets/page-peek.php
@@ -5,7 +5,7 @@
 			<a href="<?php echo $subpage->url() ?>">
 				<h3><?php echo $subpage->title() ?></h3>
 				<?php if ($subpage->hasImages()): ?>	
-					<img src="<?php echo $subpage->images()->first()->crop('360')->url() ?>" alt="">
+					<img src="<?php echo $subpage->images()->first()->crop('408', '263')->url() ?>" alt="">
 				<?php endif ?>
 			</a>
 		<?php endforeach ?>		
@@ -15,17 +15,28 @@
 			<a href="<?php echo $subpage->url() ?>">
 				<h3><?php echo $subpage->title() ?></h3>
 				<?php if ($subpage->hasImages()): ?>	
-					<img src="<?php echo $subpage->images()->first()->crop('360')->url() ?>" alt="">
+					<img src="<?php echo $subpage->images()->first()->crop('408', '263')->url() ?>" alt="">
 				<?php endif ?>
 			</a>
 		<?php endforeach ?>
 	<?php endif ?>
+<?php elseif($section == 'studio'): ?>
+	<?php foreach ($pages->find($section)->children()->sortBy('date', 'desc')->limit(1) as $subpage): ?>
+	<a href="<?php echo $subpage->url() ?>">
+		<h3><?php echo $subpage->title() ?></h3>
+
+		<?php if ($subpage->hasImages()): ?>	
+			<img src="<?php echo $subpage->images()->first()->crop('408', '263')->url() ?>" alt="">
+		<?php endif ?>
+	</a>
+	<?php endforeach ?>	
 <?php else: ?>
 	<?php foreach ($pages->find($section)->children()->sortBy('date', 'desc')->limit(1) as $subpage): ?>
 	<a href="<?php echo $subpage->url() ?>">
 		<h3><?php echo $subpage->title() ?></h3>
+
 		<?php if ($subpage->hasImages()): ?>	
-			<img src="<?php echo $subpage->images()->first()->crop('360')->url() ?>" alt="">
+			<img src="<?php echo $subpage->images()->first()->crop('263')->url() ?>" alt="">
 		<?php endif ?>
 	</a>
 	<?php endforeach ?>
--- site/templates/home.php
+++ site/templates/home.php
@@ -5,14 +5,18 @@
 </div>
 
 <div class="row peek">
-	<div class="col-xs-6 col-sm-5">
-		<?php snippet('page-peek', array('section' => 'events')) ?>
+	<div class="col-xs-6 col-sm-9">
+		<div class="row">
+			<div class="col-xs-6 col-sm-6">
+				<?php snippet('page-peek', array('section' => 'events')) ?>
+			</div>
+			<div class="col-xs-6 col-sm-6">
+				<h2>Recess Studio</h2>
+				<?php snippet('page-peek', array('section' => 'studio')) ?>
+			</div>
+		</div>
 	</div>
-	<div class="col-xs-6 col-sm-5">
-		<h2>Recess Studio</h2>
-		<?php snippet('page-peek', array('section' => 'studio')) ?>
-	</div>
-	<div class="col-xs-6 col-sm-2">
+	<div class="col-xs-6 col-sm-3">
 		<h2>Intermission</h2>
 		<?php snippet('page-peek', array('section' => 'intermission')) ?>
 	</div>
