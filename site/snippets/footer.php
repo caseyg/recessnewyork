0224b32a862131f96cd4f82c79aefc50fd78f2dd
Casey Gollan <casey@bullshit.systems>
2016-01-30 13:32:05 -0500

    footer form fixes #34 and font tweaks

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,24 +1,22 @@
 		<hr>
 		<footer class="row">
 			<!-- Begin MailChimp Signup Form -->
-			<div id="mc_embed_signup" class="form-inline col-xs-12 col-sm-7">
-			<form action="//recessnewyork.us3.list-manage.com/subscribe/post?u=d7fd7e8a4f6eebf73bf06bdc2&amp;id=e18659fdfb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-			    <div id="mc_embed_signup_scroll">
+			<div id="mc_embed_signup" class="form-inline col-xs-12 col-sm-8">
+				<form action="//recessnewyork.us3.list-manage.com/subscribe/post?u=d7fd7e8a4f6eebf73bf06bdc2&amp;id=e18659fdfb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 					<div class="mc-field-group form-group">
-						<label for="mce-EMAIL"><i class="fa fa-envelope"></i> E-mail Sign Up</label>
-						<input type="email" placeholder="Enter e-mail address" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL">
-						<input type="submit" value="Go" name="subscribe" id="mc-embedded-subscribe" class="btn btn-default">
+						<label for="mce-EMAIL" class="col-xs-12"><i class="fa fa-envelope"></i> E-mail Sign Up</label>
+						<span class="col-xs-10"><input type="email" placeholder="Enter e-mail address" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL"></span>
+						<input type="submit" value="Go" name="subscribe" id="mc-embedded-subscribe" class="btn btn-default col-xs-2">
 					</div>
-					<div id="mce-responses">
+					<div id="mce-responses col-xs-12">
 						<div class="response" id="mce-error-response" style="display:none"></div>
 						<div class="response" id="mce-success-response" style="display:none"></div>
 					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 				    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d7fd7e8a4f6eebf73bf06bdc2_e18659fdfb" tabindex="-1" value=""></div>
-			    </div>
-			</form>
+				</form>
 			</div>
 			<!--End mc_embed_signup-->
-			<ul class="follow list-inline col-xs-12 col-sm-5">
+			<ul class="follow list-inline col-xs-12 col-sm-4">
 				<li><strong>Follow Us:</strong></li>
 				<li><a href="http://facebook.com/recessnewyork"><i class="fa fa-facebook"></i></a></li>
 				<li><a href="http://instagram.com/khairimdnor"><i class="fa fa-instagram"></i></a></li>
