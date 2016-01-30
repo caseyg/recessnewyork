83598bf77651f965520c708e64004239b25af861
Casey Gollan <casey@bullshit.systems>
2016-01-30 13:03:14 -0500

    stack next event and studio, fixes #32

--- site/templates/home.php
+++ site/templates/home.php
@@ -7,10 +7,10 @@
 <div class="row peek">
 	<div class="col-xs-12 col-sm-9">
 		<div class="row">
-			<div class="col-xs-6 col-sm-6">
+			<div class="col-xs-12 col-sm-6">
 				<?php snippet('page-peek', array('section' => 'events')) ?>
 			</div>
-			<div class="col-xs-6 col-sm-6">
+			<div class="col-xs-12 col-sm-6">
 				<h2>Recess Studio</h2>
 				<?php snippet('page-peek', array('section' => 'studio')) ?>
 			</div>
