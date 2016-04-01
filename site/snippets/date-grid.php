8126f9007bc4c3d3c1b2b5dd6fa63e372864d01c
Casey Gollan <hello@caseyagollan.com>
2016-03-31 21:03:14 -0400

    sort mixes by order field

--- site/snippets/date-grid.php
+++ site/snippets/date-grid.php
@@ -5,8 +5,8 @@
 			<?php foreach($months as $month => $uris): ?>
 			<div class="row">
 				<h2 class="col-xs-12"><?php echo date("F", mktime(0, 0, 0, $month)) ?> <?php echo $year ?></h2>
-				<?php foreach($uris as $uri): ?>
-					<?php $post = $pages->find($uri); ?>
+				<?php $sortedPages = pages($uris); ?>
+				<?php foreach($sortedPages->sortBy('order', 'asc') as $post): ?>
 					<a class="col-xs-6 col-sm-3" href="<?php echo $post->url() ?>">
 						<img src="<?php if($post->images()->find('thumbnail.jpg')): ?><?php echo $post->images()->find('thumbnail.jpg')->url() ?><?php else: ?><?php if($post->images()->count() > 0): ?><?php echo $post->images()->first()->url() ?><?php else: ?>http://placehold.it/375x375<?php endif ?><?php endif ?>" alt="">
 						<p><?php echo $post->title() ?></p>
